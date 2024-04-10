6101db44a9f3259ae3b67cbc9cbdbab578266b4b
wojtaz31 <wojtaz31@wp.pl>
2024-04-10 12:52:04 +0200

    added function, that goes through video and returns frame numbers, in which face matches appeared

---
+++ videoRecog.py
@@ -0,0 +1,33 @@
+import cv2
+from deepface import DeepFace
+
+video_matches_timestamps = []
+
+def find_match(frame, ref_img, frame_number):
+    try:
+        if DeepFace.verify(frame, ref_img.copy(), enforce_detection=False)['verified']:
+            video_matches_timestamps.append((frame_number))
+    except ValueError:
+        pass
+
+
+def videoRecog(reference_path, videoPath=''):
+    frame_number = 0
+    ref_img = cv2.imread(reference_path)
+    cap = cv2.VideoCapture(videoPath)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 320)
+    while True:
+        ret, frame = cap.read()
+        frame_number += 1
+        if not ret:
+            break
+
+        if frame_number % 15 == 0:
+            find_match(frame.copy(), ref_img, frame_number)
+
+    print("Frames:")
+    print(video_matches_timestamps)
+    cap.release()
+
+videoRecog('/reference.jpg', '/video-test.mp4')
